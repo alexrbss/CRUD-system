cd8997a3770a6aac55ece00e20ae8cc215e3edc9
alexrbss <alexandre.rbss@gmail.com>
2025-10-11 16:40:57 -0300

    feat: classe DataBaseConnection adicionada

---
+++ database/DataBaseConnection.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/UserDB";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver PostgreSQL não encontrado", e);
+        }
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
+    }
+}
